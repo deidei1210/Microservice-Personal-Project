d055d6dad6d3915b97a571cc11ff86c5e8a8b837
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-10-21 15:28:53 +0800

    上传已经写好的内容

---
+++ src/components/map.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="map">
+    <baidu-map class="map" :center="{ lng: 116.404, lat: 39.915 }" :zoom="15">
+    </baidu-map>
+  </div>
+  <div>你好！</div>
+</template>
+
+<script>
+/* eslint-disable */
+import { BaiduMap } from "vue-baidu-map-3x";
+import { ref, onMounted } from "vue";
+
+export default {
+  components: {
+    BaiduMap,
+  },
+};
+
+const center = ref({ lng: 0, lat: 0 });
+const zoom = ref(3);
+
+const handler = ({ BMap, map }) => {
+  console.log(BMap, map);
+  center.value.lng = 116.404;
+  center.value.lat = 39.915;
+  zoom.value = 15;
+};
+</script>
+
+<style>
+.map {
+  width: 100%;
+  height: 300px;
+}
+</style>
