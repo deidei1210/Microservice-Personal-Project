91fc026cfa75172a4dee07646c0c504804bab41e
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-10-23 15:01:47 +0800

    成功调用GPT接口

--- src/components/map.vue
+++ src/components/map.vue
@@ -13,7 +13,7 @@
         :key="index"
         :position="marker.position"
         :dragging="false"
-        @click="infoWindowOpen(index)"
+        @click="toggleMarkerAnimation(index)"
       >
         <bm-label
           :content="marker.content"
@@ -27,6 +27,16 @@
         <button @click="zoomOut">缩小</button>
       </div>
     </baidu-map>
+  </div>
+
+  <!-- 用于显示天气 -->
+  <div class="weather-container" v-if="weatherData">
+    <p>{{ weatherData.lives[0].city }}实时天气</p>
+    <p>温度：{{ weatherData.lives[0].temperature }}°C</p>
+    <p>天气：{{ weatherData.lives[0].weather }}</p>
+    <p>湿度：{{ weatherData.lives[0].humidity }}%</p>
+    <p>风向：{{ weatherData.lives[0].winddirection }}风</p>
+    <p>风力：{{ weatherData.lives[0].windpower }}级</p>
   </div>
 </template>
 
@@ -43,6 +53,8 @@ import {
 } from "vue-baidu-map-3x";
 import { ref, onMounted } from "vue";
 import axios from "axios";
+// import { Configuration, OpenAIApi } from "openai"; // 根据实际情况调整库的导入路径
+import OpenAI from "openai";
 
 export default {
   components: {
@@ -226,14 +238,20 @@ export default {
           show: false,
         },
       ],
-      weatherData: null,
+      weatherData: null, //存储天气数据
       city: "310000", //上海
+      selectedMarkerIndex: -1, // 记录选中的标记索引
     };
   },
   created() {
     this.getWeatherData();
     this.searchWiki("上海");
     console.log(this.weatherData);
+    this.initGPT3();
+  },
+  mounted() {
+    this.mapInstance = this.$refs.map ? this.$refs.map.getMap() : null;
+    this.getWeatherData();
   },
   methods: {
     //通过点击地图上的点获得该点具体的地点名称
@@ -282,29 +300,58 @@ export default {
           console.error(error);
         });
     },
+    //调用Wiki百科API
     async searchWiki(keyword) {
-      try{
-        const response=await axios.get('https://zh.wikipedia.org/w/api.php',{
-          params:{
-            format:'json',
-            action:'query',
-            generator:'search',
-            gsrnamespace:0,
-            gsrlimit:10,
-            prop:'pageimages|extracts',
-            pilimit:'max',
-            exintro:true,
-            explaintext:true,
-            exsentences:1,
-            exlimit:'max',
-            origin:'*',
-            gsrsearch:"上海南京东路"
-          }
+      try {
+        const response = await axios.get("https://zh.wikipedia.org/w/api.php", {
+          params: {
+            format: "json",
+            action: "query",
+            generator: "search",
+            gsrnamespace: 0,
+            gsrlimit: 10,
+            prop: "pageimages|extracts",
+            pilimit: "max",
+            exintro: true,
+            explaintext: true,
+            exsentences: 1,
+            exlimit: "max",
+            origin: "*",
+            gsrsearch: "上海南京东路",
+          },
         });
         console.log(response.data.query.pages);
-      }catch(error){
-        console.error('Error:',error);
+      } catch (error) {
+        console.error("Error:", error);
       }
+    },
+    //点击标记物之后标记开始跳动
+    toggleMarkerAnimation(index) {
+      if (!this.mapInstance) return; // 地图未初始化，不进行操作
+
+      this.selectedMarkerIndex = index; // 更新选中的标记索引
+
+      this.markers.forEach((marker, i) => {
+        if (i === index) {
+          marker.marker.setAnimation(BMAP_ANIMATION_BOUNCE); // 设置BOUNCE动画
+        } else {
+          marker.marker.setAnimation(null); // 取消动画
+        }
+      });
+    },
+    async initGPT3(details) {
+      const API_KEY = "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"; //输入API Key
+      const openai = new OpenAI({
+        apiKey: API_KEY,
+        dangerouslyAllowBrowser: true,
+      });
+
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: `你好，请问可以给我介绍一下上海的 ${details} 吗？用200个字概括！`  }],
+        model: "gpt-3.5-turbo",
+      });
+      console.log(chatCompletion);
+      console.log(chatCompletion.choices[0].message.content)
     },
   },
 };
@@ -320,5 +367,15 @@ export default {
   top: 10px;
   left: 10px;
   z-index: 9999;
+}
+.weather-container {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 9999;
+  background-color: #b0e2ff; /* 设置背景颜色为浅蓝色 */
+  border-radius: 10px; /* 设置圆角边框半径为10px */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 设置阴影效果 */
+  /* 添加其他样式 */
 }
 </style>
